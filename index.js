38b0d942190f7bfb4eb23de68bae9c8aaeb2ba04
ehroy <68073896+ehroy@users.noreply.github.com>
2024-05-23 09:40:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const TURBO = require("./lib/otp");
 const sleep = require("delay");
+const inquirer = require("inquirer");
 (async () => {
+  let choicess = [];
+  const sms = new TURBO("c657c69a4cdde52eecc8feeb6f9a886b");
+  const services = await sms.GetServis();
+  services.data.data.forEach(async (element, index) => {
+    await choicess.push(element);
+  });
+  choicess.push("cancel");
+  let selectvoucher = await inquirer
+    .prompt([
+      {
+        type: "rawlist",
+        choices: choicess,
+        name: "selected",
+        message: "Select Your Choices ?",
+      },
+    ])
+    .then((answers) => {
+      return answers.selected;
+    });
+
+  if (selectvoucher == "cancel") {
+    console.log("[CANCELED]");
+    process.exit(0);
+  }
+  let obj = choicess.find((o, i) => {
+    if (o.name.includes(selectvoucher)) {
+      return choicess[i];
+    }
+  });
+
   while (true) {
+    let id;
     let dataTurbo;
     let smsasu;
     let otpCodeTurbo;
-    const sms = new TURBO("");
 
     do {
       try {
         await sleep(1000);
-        dataTurbo = await sms.GetNumber("51");
+        dataTurbo = await sms.GetNumber(obj.id);
       } catch (err) {
         console.log(`Gagal Mendapatkan Nomer ${err}`);
         await sleep(5000);
@@ -20,23 +51,28 @@ const sleep = require("delay");
     } while (dataTurbo.success === false);
 
     let { order_id, number } = dataTurbo.data.data;
-    console.log("succes get number", number);
+    id = order_id;
+    console.log("succes get number", number, "order id", id);
     console.log("Try get message");
+    let count = 0;
     try {
       do {
         try {
-          otpCodeTurbo = await sms.GetMessage(order_id);
+          otpCodeTurbo = await sms.GetMessage(id);
 
           if (count === 60) {
-            await sms.GetCancel(order_id);
+            await sms.GetCancel(id);
           }
           await sleep(1000);
           count++;
         } catch (error) {
           break;
         }
-        // console.log(otpCodeTurbo.data.data[0]);
-      } while (otpCodeTurbo.data.data[0].sms === null);
+        process.stdout.write(`Waiting Otp [ ${count} second ]...\r`);
+      } while (
+        otpCodeTurbo.data.data[0].sms === null ||
+        otpCodeTurbo.data.data[0].sms === "null"
+      );
       smsasu = otpCodeTurbo.data.data[0];
     } catch (error) {
       await sms.GetCancel(order_id);
@@ -50,8 +86,10 @@ const sleep = require("delay");
     } else {
       try {
         const parse = JSON.parse(smsasu.sms);
-        console.log("Message =>", parse);
+
+        console.log("Message =>", parse[0].sms);
       } catch (error) {
+        await sms.GetCancel(id);
         console.log(error);
         console.log("Cancel Phone Number");
       }
